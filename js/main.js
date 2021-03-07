553629872d7b04fc14ddf18c0582ea300bd17485
JoelB-E276 <joel.beauroy@outlook.fr>
2021-03-07 11:04:41 +0100

    Première partie

--- js/main.js
+++ js/main.js
@@ -9,20 +9,21 @@ alert ("Bienvenue dans le jeu du Pendu ");
     let arrayWords = ["pollution", "foret" , "arbre" ,"pesticide" , "eolienne" , "proteger","ecosysteme"] ; // tableau des mots
 
 
-    let cptChoice = arrayWords [Math.floor(Math.random() * arrayWords.length)]; // choix aléatoire des mots
-    let sizeWord = cptChoice.length;
+let cptChoice = arrayWords [Math.floor(Math.random() * arrayWords.length)]; // choix aléatoire des mots
+let sizeWord = cptChoice.length;
 
   console.log(cptChoice + score); // Pour vérifier le mot lors de la construction du code.
 
 prompt(`Choisis une lettre et tente de découvrir le mot caché : ${"_ ".repeat(cptChoice.length)}`)// Affiche des underscores à la place du nombre de lettre.
 let splitWord = cptChoice.split("");
-let playerChoice = ["b","o","a","e"];
+let playerChoice = ("b","o","a","e");
 console.log (splitWord +" " +score); // Pour vérifier le mot lors de la construction du code.
-
 let tape = [];      // Choix du joueur en cours
-let re = splitWord.includes(tape);
-console.log(re);
-if (splitWord.includes(tape)) ;
+let re = splitWord.includes(playerChoice.length); // renvoi true/false de manière aléatoire. ?
+
+console.log(re);  
+
+    if (splitWord.includes(playerChoice.length)) ;
 
 
  
@@ -32,6 +33,6 @@ function counter(){     // Comptage du score et fin du jeu en cours
          penality;
     }  
     if (score === 0){
-        return alert(" PEERDU ! Tu as été pendu!! ") 
+        return alert(" PERDU ! Tu as été pendu!! ") 
     }
 }
