63e804ea3a035e61a390864426855e004b950c17
JoelB-E276 <joel.beauroy@outlook.fr>
2021-03-07 15:06:51 +0100

    Index change

--- js/main.js
+++ js/main.js
@@ -16,12 +16,3 @@ console.log(cptChoice + score); // Pour vérifier le mot lors de la construction
 prompt(`Choisis une lettre et tente de découvrir le mot caché : ${"_ ".repeat(cptChoice.length)}`)// Affiche des underscores à la place du nombre de lettre.
 
 //////////////////////  en cours de construction  /////////////////////
-
-
-let splitWord = cptChoice.split("");
-let playerChoice = ("b","o","a","e"); // Test alphabet pour choix des lettres 
-console.log (splitWord +" " +score); // Pour vérifier le mot lors de la construction du code.
-let tape = [];      // Choix du joueur en cours
-let re = splitWord.includes(i.length); // renvoi true/false de manière aléatoire. ?
-
-console.log(re);  
